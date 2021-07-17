1894ff1b08d8828bd32bc18add67a9ad30a3fda5
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-17 15:19:49 +0300

    Add Advertisement Repository

---
+++ DataAccess/Repository/AdvertisementRepository.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using DataAccess.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class AdvertisementRepository : BaseRepository<Advertisement>, IAdvertisementRepository
+    {
+        public AdvertisementRepository(AdvertisementContext context) : base(context) { }
+        async Task<IReadOnlyCollection<Advertisement>> IAdvertisementRepository.FindAdvertisementByConditionAllIncludedAsync(Expression<Func<Advertisement, bool>> advertisementPredicate)
+        {
+            return await Entities.Where(advertisementPredicate).ToListAsync().ConfigureAwait(false);           
+        }
+
+        async Task<IReadOnlyCollection<Advertisement>> IAdvertisementRepository.FindAllAdvertisementsAllIncludedAsync()
+        {
+            return await Entities.ToListAsync().ConfigureAwait(false);
+        }
+
+        async Task<Advertisement> IAdvertisementRepository.GetAdvertisementAllIncludedAsync(Expression<Func<Advertisement, bool>> advertisementPredicate)
+        {
+            return await AdvertisementContext.Advertisements.Where(advertisementPredicate).FirstOrDefaultAsync();
+        }
+        public override async Task<Advertisement> CreateAsync(Advertisement advertisement)
+        {
+            await AdvertisementContext.Advertisements.AddAsync(advertisement);
+            await AdvertisementContext.SaveChangesAsync();
+            return advertisement;
+        }
+    }
+}
