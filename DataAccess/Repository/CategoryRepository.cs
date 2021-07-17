cff7521b00b5200335ec378a23dae86bfe47ec6d
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-17 20:24:49 +0300

    Category repository

---
+++ DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using DataAccess.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategory
+    {
+        public CategoryRepository(AdvertisementContext context) : base(context)
+        {
+        }
+
+        async Task<IReadOnlyCollection<Category>> ICategory.FindAllCategorysAllIncludedAsync()
+        {
+            return await Entities.ToListAsync().ConfigureAwait(false);
+        }
+
+        async Task<IReadOnlyCollection<Category>> ICategory.FindCategoryByConditionAllIncludedAsync(Expression<Func<Category, bool>> categoryPredicate)
+        {
+            return await Entities.Where(categoryPredicate).ToListAsync().ConfigureAwait(false);
+        }
+
+        async Task<Category> ICategory.GetCategoryAllIncludedAsync(Expression<Func<Category, bool>> categoryPredicate)
+        {
+            return await AdvertisementContext.Categories.Where(categoryPredicate).FirstOrDefaultAsync();
+        }
+
+        public override async Task<Category> CreateAsync(Category category)
+        {
+            await AdvertisementContext.Categories.AddAsync(category);
+            await AdvertisementContext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
