5d968fc9b786ac2ace6eaad12fc7e075366e4c3c
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-17 14:56:27 +0300

    Category interface with extra function

---
+++ DataAccess/Repository/Interfaces/IСategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Interfaces
+{
+    public interface IСategory : IRepository<Models.Category>
+    {
+        Task<IReadOnlyCollection<Models.Category>> FindAllCategorysAllIncludedAsync();
+        Task<IReadOnlyCollection<Models.Category>> FindCategoryByConditionAllIncludedAsync(Expression<Func<Models.Category, bool>> categoryPredicate);
+        Task<Models.Category> GetCategoryAllIncludedAsync(Expression<Func<Models.Category, bool>> taskPredicate);
+    }
+}
