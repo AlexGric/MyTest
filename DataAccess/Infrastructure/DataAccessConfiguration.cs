7479f4265194b688ff0d77f10c9026c04a0db812
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-17 20:39:49 +0300

    Data Access configuration and transients Category and Advertisement

---
+++ DataAccess/Infrastructure/DataAccessConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Context;
+using DataAccess.Repository;
+using DataAccess.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.Infrastructure
+{
+    public static class DataAccessConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient(typeof(IAdvertisementRepository), typeof(AdvertisementRepository));
+            services.AddTransient(typeof(ICategory), typeof(CategoryRepository));
+
+            services.AddDbContext<AdvertisementContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("AdvertisementContext")));
+        }
+    }
+}
