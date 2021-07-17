6fb7f22216fca6fe913f581bed6ddd51920e6a2a
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-17 14:52:00 +0300

    Advertisment interfase with extra functions

---
+++ DataAccess/Repository/Interfaces/IAdvertisementRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository.Interfaces
+{
+    public interface IAdvertisementRepository : IRepository<Models.Advertisement>
+    {
+        Task<IReadOnlyCollection<Models.Advertisement>> FindAllAdvertisementsAllIncludedAsync();
+        Task<IReadOnlyCollection<Models.Advertisement>> FindAdvertisementByConditionAllIncludedAsync(Expression<Func<Models.Advertisement, bool>> advertisementPredicate);
+        Task<Models.Advertisement> GetAdvertisementAllIncludedAsync(Expression<Func<Models.Advertisement, bool>> taskPredicate);
+    }
+}
