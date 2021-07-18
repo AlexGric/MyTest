750c41b68311a396c47199115b704b3d98dc8f61
Алексей Гриценко <aleshagritsenko@ukr.net>
2021-07-18 14:11:53 +0300

    Advertisement View models

---
+++ BusinessLogic/ViewModels/Advertisement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.ViewModels
+{
+    public enum AdType
+    {
+        TextAd,
+        HtmlAd,
+        BannerAd,
+        VideoAd
+    }
+
+    public class Advertisement
+    {
+        public AdType AdType { get; set; }
+        public Category Category { get; set; }
+        public int Cost { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+    }
+}
